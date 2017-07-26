f69f4da0a30416531895c741bd9bf9dc3266167e
Tim Suchanek <tim.suchanek@gmail.com>
2017-07-26 14:37:04 +0200

    Added example

--- frontend/src/examples.ts
+++ frontend/src/examples.ts
@@ -17,7 +17,7 @@ console.log(screenshot)
 
 await chromeless.end()`,
 }, {
-  title: 'Google Search',
+  title: 'Google Search Screenshot',
   code: `\
 const chromeless = new Chromeless({ remote: true })
 
@@ -32,6 +32,31 @@ const screenshot = await chromeless
 console.log(screenshot)
 
 await chromeless.end()`,
+}, {
+  title: 'Extract Google Search Results',
+  code: `\
+  const chromeless = new Chromeless({ remote: true })
+
+const links = await chromeless
+  .goto('https://www.google.com')
+  .type('chromeless', 'input[name="q"]')
+  .press(13)
+  .wait('#resultStats')
+  .evaluate(() => {
+    // this will be executed in headless chrome
+    const links = [].map.call(
+      document.querySelectorAll('.g h3 a'),
+      a => ({title: a.innerText, href: a.href})
+    )
+    return JSON.stringify(links)
+  })
+  // you can still use the method chaining API after evaluating
+  // when you're done, at any time you can call \`.then\` (in our case \`await\`)
+  .scrollTo(0, 1000)
+
+console.log(links)
+
+await chromeless.end()`
 }]
 
 export default examples
